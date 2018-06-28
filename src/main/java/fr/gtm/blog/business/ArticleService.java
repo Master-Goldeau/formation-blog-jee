6119d9826d293780e802dc0e4bdc0f779eb197e7
Etienne <etienne.savary@gmail.com>
2018-06-28 09:19:23 +0200

    MAJ avec la version Elvynia de ce jour

--- src/main/java/fr/gtm/blog/business/ArticleService.java
+++ src/main/java/fr/gtm/blog/business/ArticleService.java
@@ -1,62 +1,85 @@
 package fr.gtm.blog.business;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import fr.gtm.blog.dao.ArticleRepository;
 import fr.gtm.blog.domain.Article;
 
-@Service
+/**
+ * Couche métier de l'application : Gérer les informations des articles,
+ * intermédiaire entre la présentation et la persistance.
+ */
 public class ArticleService {
 
-	private static final List<Article> MOCK_ARTICLES = Collections
-			.unmodifiableList(Arrays.asList(
-					new Article[] { new Article(0, "Article n°1", "DESCR"),
-							new Article(1, "Article n°2", "DESCR"),
-							new Article(2, "Article n°3", "DESCR") }));
-
+	/**
+	 * Une instance de l'implémentation de ArticleRepository (générée par
+	 * Spring) sera injectée en valeur de "repo" grâce à cette annotation.
+	 */
 	@Autowired
 	private ArticleRepository repo;
-	private int idCount;
-	private final List<Article> articles;
 
-	public ArticleService(int idCount) {
-		this.idCount = idCount;
-		this.articles = new ArrayList<>();
-		// FIXME: A supprimer lorsque l'accès BDD est implémenté.
-		this.articles.addAll(ArticleService.MOCK_ARTICLES);
+	/**
+	 * Créer et enregister un nouvel article.
+	 * 
+	 * @param title le titre du nouvel article.
+	 * @param description sa description associée.
+	 * @return Article l'article créé avec son identifiant rempli.
+	 */
+	public Article create(final String title, final String description) {
+		return this.save(new Article(title, description));
 	}
 
-	public Article create(final String title, final String description) {
-		final Article result = new Article();
-		// result.setId(this.idCount++);
-		result.setTitle(title);
-		result.setDescription(description);
-		// this.articles.add(result);
-		this.repo.save(result);
+	/**
+	 * Récupération de la liste des article depuis la persistence.
+	 * 
+	 * @return List<Article> tous les articles en BDD.
+	 */
+	public List<Article> getArticles() {
+		return this.repo.findAll();
+	}
+
+	public Article read(Integer id) {
+		Article result = null;
+		final Optional<Article> wrapper = this.repo.findById(id);
+		if (wrapper.isPresent()) {
+			result = wrapper.get();
+		}
 		return result;
 	}
 
-	public List<Article> getArticles() {
-		articles.addAll(this.repo.findAll());
-		return articles;
+	/**
+	 * Supprime un article.
+	 * 
+	 * @param id l'identifiant de l'article à supprimer.
+	 */
+	public void delete(int id) {
+		this.repo.deleteById(id);
 	}
 
-	public void delete(int id) {
-		Article toDelete = null;
-		for (Article a : this.articles) {
-			if (a.getId().equals(id)) {
-				toDelete = a;
-				break;
-			}
+	/**
+	 * Mise à jour d'un article existant en BDD.
+	 * 
+	 * @param article l'article à mettre à jour avec id rempli.
+	 * @return Article l'article mis à jour depuis la BDD.
+	 */
+	public Article edit(Article article) {
+		if (article.getId() == null) {
+			throw new IllegalArgumentException(
+					"Cannot update an article without its id.");
 		}
-		if (toDelete != null) {
-			this.articles.remove(toDelete);
-		}
+		return this.save(article);
+	}
+
+	/**
+	 * Fonction permettant de factoriser les appels à repo.save(..).
+	 * 
+	 * @param article l'article à persister pour création ou mise à jour.
+	 * @return Article l'article modifié et validé par la BDD.
+	 */
+	private Article save(Article article) {
+		return this.repo.save(article);
 	}
 }
